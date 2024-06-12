72528beb5f1d9d10aecbac829cc741be43ce2fc4
Eshan Chathuranga <171761693+eshanchathuranga@users.noreply.github.com>
2024-06-12 14:23:57 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,10 @@ const fs = require("fs");
 global.prefix = ["."]; 
 
 //       
-global.ownerNumber = ["+94704274987"];
+global.ownerNumber = [""];
 global.OwnerName = "Eshan Chathuranga";
 global.BotName = "Sky7-Bot";
 
 
 
-global.firebase = { 
-        apiKey: "AIzaSyBIfRbDc0wZ65h8beoumSUWEbCfr3a6k6I",
-        authDomain: "paper-database-345de.firebaseapp.com",
-        projectId: "paper-database-345de",
-        storageBucket: "paper-database-345de.appspot.com",
-        messagingSenderId: "524665939881",
-        appId: "1:524665939881:web:e9f1abb923a67ab23906b8"}
+global.firebase = {}
